5575f60f9a56bb39c797f445b17afef9b425c0ac
Ch3shireDev <thesmilingcatofcheshire@gmail.com>
2017-03-14 00:09:16 +0100

    Small update
    I'm still struggling with creating object-oriented version of an
    algorithm. So far I tried graph approach, but I need to change my point
    of view for potentials.

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ import matplotlib.pyplot as plt
 dt = 0.001
 N = 10000
 
+
+
+V0 = 100.
+
+r0 = 1.
+r1 = 1.
+r2 = 1.
+r3 = 1.
+
+
 I0 = 0.
 I1 = 0.
 I2 = 0.
 I3 = 0.
-
-V0 = 100.
 
 V1 = 0.
 V2 = 0.
@@ -20,15 +28,9 @@ V5 = 0.
 V6 = 0.
 V7 = 0.
 
-r0 = 1.
-r1 = 1.
-r2 = 1.
-r3 = 1.
-
 Tab0 = np.array([])
 Tab1 = np.array([])
 Tab2 = np.array([])
-Tab3 = np.array([])
 
 for i in range(N):
     
@@ -57,128 +59,82 @@ for i in range(N):
 
     Tab0 = np.append(Tab0, I0)
     Tab1 = np.append(Tab1, I1)
-    Tab2 = np.append(Tab2, I2)
-    Tab3 = np.append(Tab3, I3)
+    Tab2 = np.append(Tab2, I3)
 
+#             |---[  ]---|
+# V0 o--[  ]--o          o---[  ]---o Ground
+#             |---[  ]---|
+#
+# All resistors ( [  ] ) have resistance 1 Ohm, V0 is 100 V
 
 Time = np.arange(N)*dt
 plt.ticklabel_format(useOffset=False, style='plain')
 plt.plot(Time, Tab0, label="1")
 plt.plot(Time, Tab1, label="2")
 plt.plot(Time, Tab2, label="3")
-plt.plot(Time, Tab3, label="4")
 plt.legend(loc='upper right', shadow=True)
 plt.show()
-
-# class Wire:
-#     R = 1.
-#     L = 1.
-#     I = 0.
-
-#     InWires = []
-#     OutWires = []
-
-#     VIn = 0.
-#     VOut = 0.
-
-#     def __init__(self):
-#         self.InWires = []
-#         self.OutWires = []
-#         self.R = 1.
-#         self.L = 1.
-#         self.I = 0.
-
-#         self.VIn = 0.
-#         self.VOut = 0.
-
-#     def TimeStep(self, flag = False, dt=0.001):
-        
-#         VOut = self.GetVOut()
-#         VIn = self.GetVIn()
-
-#         if len(self.InWires) == 0:
-#             DeltaV = VIn - VOut
-#             self.I += (DeltaV - self.I*self.R)*dt/self.L
-
-#         else:
-#             self.I = 0.
-#             for wire in self.InWires:
-#                 self.I += wire.I
-
-#         if len(self.InWires) == 0 and len(self.OutWires) == 0:
-#             return
-
-#         if len(self.OutWires) != 0:
-#             self.VOut = VIn - self.I*self.R
-#         if len(self.InWires) != 0:
-#             self.VIn = VOut + self.I*self.R
-
-#     def GetVOut(self):
-#         if len(self.OutWires) == 0:
-#             return self.VOut
-#         else:
-#             return self.OutWires[0].VIn
-
-#     def GetVIn(self):
-#         if len(self.InWires) == 0:
-#             return self.VIn
-#         else:
-#             return self.InWires[0].VOut
-
-#     def AddInWire(self, In):
-#         self.InWires.append(In)
-
-#     def AddOutWire(self, Out):
-#         self.OutWires.append(Out)
-
-# a = Wire()
-# a.VIn = 100
-
-# b = Wire()
-# c = Wire()
-# d = Wire()
-
-# a.AddOutWire(b)
-# a.AddOutWire(c)
-
-# b.AddInWire(a)
-# b.AddOutWire(d)
-
-# c.AddInWire(a)
-# c.AddOutWire(d)
-
-# d.AddInWire(b)
-# d.AddInWire(c)
-
-# for i in range(N):
-#     a.TimeStep()
-#     b.TimeStep()
-#     c.TimeStep()
-#     d.TimeStep()
-
-#     Tab0 = np.append(Tab0, a.I)
-#     Tab1 = np.append(Tab1, b.I)
-#     Tab2 = np.append(Tab2, c.I)
-#     Tab3 = np.append(Tab3, d.I)
-
-# Time = np.arange(N)*dt
-# plt.ticklabel_format(useOffset=False, style='plain')
-# plt.plot(Time, Tab0, label="1")
-# plt.plot(Time, Tab1, label="2")
-# plt.plot(Time, Tab2, label="3")
-# plt.plot(Time, Tab3, label="4")
-# plt.legend(loc='upper right', shadow=True)
-# plt.show()
-
-# plt.cla()
-
-# # exit(0)
-
-# Q2 = 0.
-
-# Tab0 = np.array([])
-# Tab1 = np.array([])
-# Tab2 = np.array([])
-# Tab3 = np.array([])
+exit(0)
 
 
+class Node:
+    
+    def __init__(self, V=0.):
+        self.V = V
+        self.Lines = []
+    def AddLine(self, line):
+        self.Lines.append(line)
+
+class Line:
+
+    def __init__(self, InNode, OutNode, R = 1.):
+        self.InNode = InNode
+        self.OutNode = OutNode
+        InNode.AddLine(self)
+        OutNode.AddLine(self)
+        self.R = R
+        self.I = 0.
+
+    def GetDeltaV(self):
+        return self.OutNode.V - self.InNode.V
+
+
+class CircuitSystem:
+
+    def __init__(self):
+        self.Nodes = []
+        self.Lines = []
+
+    def AddNode(self, V = 0.):
+        node = Node(V)
+        self.Nodes.append(node)
+        return node
+
+    def AddLine(self, InNode, OutNode, R = 1.):
+        line = Line(InNode, OutNode, R)
+        self.Lines.append(line)
+        return line
+
+    def TimeStep(self, dt=0.001):
+        for node in self.Nodes:
+            for line in node.Lines:
+                if line.InNode is node:
+                    line.I += (line.GetDeltaV() - line.R*line.I)*dt
+                    if len(line.InNode.Lines) > 1:
+                        pass
+                        # line.InNode.V = 
+
+
+System = CircuitSystem()
+
+a = System.AddNode(100.)
+b = System.AddNode()
+c = System.AddNode()
+d = System.AddNode(0.)
+
+System.AddLine(a, b)
+System.AddLine(b, c)
+System.AddLine(b, c)
+System.AddLine(c, d)
+
+System.TimeStep()
