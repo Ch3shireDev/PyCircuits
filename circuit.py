ae8c9058be0e8418b90cde29e1303b5bf73f9e8c
Ch3shireDev <thesmilingcatofcheshire@gmail.com>
2017-03-15 00:26:56 +0100

    Preparation
    I'm preparing project to switch from directed graph to undirected, I
    want to have some version to be able to go back to.

--- circuit.py
+++ circuit.py
@@ -2,17 +2,26 @@
 #parameter, so it's just complicated simulation of simple mid-school resistors system.
 
 class Line:
+
     def __init__(self, NodeIn, NodeOut, R = 1.):
         self.NodeIn = NodeIn
         self.NodeOut = NodeOut
         self.R = R
         self.I = 0.
+        self.IsChecked = False
+
     def TimeStep(self, dt = 0.001):
+        if self.IsChecked is True:
+            return
+        self.SetChecked()
         self.I += (self.DeltaV() - self.R*self.I)*dt
+    
     def VOut(self):
         return self.NodeOut.V
+    
     def VIn(self):
         return self.NodeIn.V
+    
     def DeltaV(self):
         return self.VIn() - self.VOut()
 
@@ -25,11 +34,25 @@ class Line:
         else:
             return self.NodeIn.V + self.I*self.R
 
-    def GetCurrent(self):
-        return self.I
+    def GetCurrent(self, Node):
+        if self.NodeIn is Node:
+            return self.I
+        else: 
+            return -self.I
+
+    def SetCurrent(self, Node, Current):
+        self.SetChecked()
+        if self.NodeIn is Node:
+            self.I = Current
+        else:
+            self.I = -Current
+
+    def SetChecked(self, flag=True):
+        self.IsChecked = flag
 
 
 class Node:
+    
     def __init__(self):
         self.V = 0.
         self.Lines = []
@@ -37,8 +60,10 @@ class Node:
 
     def SetConstant(self, flag = True):
         self.IsConstant = flag
+    
     def AddWire(self, line):
         self.Lines.append(line)
+    
     def Recalculate(self):
         if self.IsConstant:
             return
@@ -90,33 +115,36 @@ class Node:
 
 
 class Circuit:
+    
     def __init__(self):
         self.Nodes = []
         self.Lines = []
 
     def AddNode(self):
-        node = Node()
-        self.Nodes.append(node)
-        return node
+        Node_ = Node()
+        self.Nodes.append(Node_)
+        return Node_
 
     def AddSource(self, V):
-        node = self.AddNode()
-        node.V = V
-        node.SetConstant()
-        return node
+        Node = self.AddNode()
+        Node.V = V
+        Node.SetConstant()
+        return Node
 
     def AddGround(self):
-        node = self.AddNode()
-        node.SetConstant()
-        return node
+        Node = self.AddNode()
+        Node.SetConstant()
+        return Node
 
     def AddWire(self, NodeIn, NodeOut, R=1.):
-        line = Line(NodeIn, NodeOut, R)
-        self.Lines.append(line)
-        NodeIn.AddWire(line)
-        NodeOut.AddWire(line)
-        return line
+        Wire = Line(NodeIn, NodeOut, R)
+        self.Lines.append(Wire)
+        NodeIn.AddWire(Wire)
+        NodeOut.AddWire(Wire)
+        return Wire
 
     def TimeStep(self, dt = 0.001):
-        for node in self.Nodes:
-            node.TimeStep(dt)
+        for Wire in self.Lines:
+            Wire.SetChecked(False)
+        for Node in self.Nodes:
+            Node.TimeStep(dt)
